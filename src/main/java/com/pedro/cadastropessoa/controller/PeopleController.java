2e1fc20c7131276a065bcdc8ecba3423f26967f4
Pedro <pedro.pinto@projuris.com.br>
2021-12-07 01:10:55 -0300

    Added Swagger

--- src/main/java/com/pedro/cadastropessoa/controller/PeopleController.java
+++ src/main/java/com/pedro/cadastropessoa/controller/PeopleController.java
@@ -3,6 +3,8 @@ package com.pedro.cadastropessoa.controller;
 import com.pedro.cadastropessoa.dto.PeopleDTO;
 import com.pedro.cadastropessoa.model.People;
 import com.pedro.cadastropessoa.service.PeopleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,12 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("api/v1/people")
 @RequiredArgsConstructor
+@Api(value = "API REST Cadastro Pessoa")
 public class PeopleController {
     private final PeopleService peopleService;
 
     @PostMapping
+    @ApiOperation(value = "Registra uma nova pessoa no sistema")
     public ResponseEntity<PeopleDTO> register(@RequestBody @Valid PeopleDTO peopleDTO) throws Exception {
         peopleService.peopleRegister(peopleDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
